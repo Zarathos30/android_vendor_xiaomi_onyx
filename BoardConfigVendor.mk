5b9e70628b37769b0545b2b7e57159ecab34d497
Zarathos30 <vale.rigni@gmail.com>
2026-09-23 19:55:42 +0200

    onyx: Convert radio files to uwu_prebuilt_image
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -33,3 +33,35 @@ AB_OTA_PARTITIONS += \
     xbl \
     xbl_config \
     xbl_ramdump
+
+AB_OTA_RADIO_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    countrycode \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    idmanager \
+    imagefv \
+    keymaster \
+    modem \
+    modemfirmware \
+    multiimgqti \
+    pvmfw \
+    qupfw \
+    shrm \
+    soccp_dcd \
+    soccp_debug \
+    spuservice \
+    tz \
+    uefi \
+    uefisecapp \
+    vm-bootsys \
+    xbl \
+    xbl_config \
+    xbl_ramdump
--- onyx-vendor.mk
+++ onyx-vendor.mk
@@ -3467,3 +3467,35 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+PRODUCT_PACKAGES += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    countrycode \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    idmanager \
+    imagefv \
+    keymaster \
+    modem \
+    modemfirmware \
+    multiimgqti \
+    pvmfw \
+    qupfw \
+    shrm \
+    soccp_dcd \
+    soccp_debug \
+    spuservice \
+    tz \
+    uefi \
+    uefisecapp \
+    vm-bootsys \
+    xbl \
+    xbl_config \
+    xbl_ramdump
